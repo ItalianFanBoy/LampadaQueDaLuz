536d1f4bd686d2ccb559d45f45b8cadd86400025
Guilherme Henrique Amaral <101069377+ItalianFanBoy@users.noreply.github.com>
2022-05-01 23:41:03 -0300

    Projetinho
    Brincando com a lampada projeto finalizado

---
+++ lampada.js
@@ -0,0 +1,33 @@
+const turnOn = document.getElementById('turnOn');
+const turnOff = document.getElementById('turnOff');
+const lampada = document.getElementById('lamp');
+
+
+function isLampBroken (){
+    return lamp.src.indexOf('quebrada') > -1;
+}
+
+function lampOn(){
+    if (!isLampBroken()){
+
+    
+    lamp.src = "imagens\ligada.jpg";
+    }
+}
+    function lampOff(){
+        if (!isLampBroken()){
+        lamp.src = "imagens\desligada.jpg"
+    }
+ }
+    function lampBroken(){
+        lamp.src = "imagens\quebrada.jpg"
+    }
+
+turnOn.addEventListener('click', lampOn);
+turnOff.addEventListener('click', lampOff);
+
+lamp.addEventListener('mouseover', lampOn);
+lamp.addEventListener('mouseleave', lampOff);
+lampOff.addEventListener('dbclick', lampBroken);
+
+
